deb69c31869cc2c07e192ceebd4600fece49b804
G Chow <grendon.chow@gmail.com>
2022-03-06 23:38:11 +0800

    Added basic functionality to perform arithmetic and display results using buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+let operandOne = '';
+let operandTwo = '';
+let operator = null;
+let func = null;
+let displayNumber = '';
+
+const input = document.querySelector('.input');
+
 const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', () => buttonPress(button.textContent));
@@ -9,20 +17,75 @@ function buttonPress(button)
     switch(true)
     {
         case(!isNaN(button)):
+            displayNumber = "" + displayNumber + button;
+            displayInput(displayNumber);
             break;
         case(button == 'Clear'):
+            clearCalc();
             break;
         case(button == 'Delete'):
             break;
         case(button  == '='):
+            operandTwo = parseInt(displayNumber);
+            displayInput(operate(operandOne, operator, operandTwo));
+            displayNumber = '';
             break;
         case(button == '+'):
+            operator = button;
+            operation();
             break;
         case(button  == '-'):
+            operator = button;
+            operation();
             break;
         case(button  == 'x'):
+            operator = button;
+            operation();
             break;
         case(button  == '÷'):
+            operator = button;
+            operation();
             break;
     }
+}
+
+//Used to prepare for next operand.
+function operation()
+{
+    displayInput('0');
+    operandOne = parseInt(displayNumber);
+    displayNumber = '';
+}
+function displayInput(num)
+{
+    input.textContent = num;
+}
+
+function clearCalc()
+{
+    operandTwo = '';
+    operandOne = '';
+    displayNumber = '';
+    operator = null;
+    displayInput("0");
+}
+
+function operate(operandOne, operator, operandTwo)
+{
+    switch(operator)
+    {
+        case '+':
+            result = operandOne + operandTwo;
+            break;
+        case '-':
+            result = operandOne - operandTwo;
+            break;
+        case 'x':
+            result = operandOne * operandTwo;
+            break;
+        case'÷':
+            result = operandOne / operandTwo
+            break;
+    }
+    return result;
 }
